29ca8bf888c23f3250fb27b4f1de7166fb99f15a
= <=>
2021-10-09 16:54:24 -0300

    Fix cors

--- src/index.js
+++ src/index.js
@@ -6,7 +6,12 @@ const { normalizePort, onError } = require('./utils')
 const port = normalizePort(process.env.PORT || 3000)
 app.set('port', port)
 
-app.use(cors())
+app.use((_, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE')
+    app.use(cors())
+    next()
+})
 
 const server = http.createServer(app)
 server.listen(port)
