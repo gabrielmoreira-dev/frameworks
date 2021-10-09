b4dfbc8ea91de5dc716d707b0fc4176b38a65102
= <=>
2021-10-09 17:03:51 -0300

    Hotfix - Cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+app.use((_, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE')
+    app.use(cors())
+    next()
+})
 
 const moviesRoutes = require("./routes/movies")
 app.use('/api/filmes', moviesRoutes)
--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 const app = require('./app')
 const http = require('http')
-const cors = require('cors')
 const { normalizePort, onError } = require('./utils')
 
 const port = normalizePort(process.env.PORT || 3000)
 app.set('port', port)
-
-app.use((_, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE')
-    app.use(cors())
-    next()
-})
 
 const server = http.createServer(app)
 server.listen(port)
