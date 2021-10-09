e63a0f118a4ab6a695f3aa30618d23026141b7ef
= <=>
2021-10-08 21:54:17 -0300

    Add cors

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const app = require('./src/app')
 const http = require('http')
+const cors = require('cors')
 const { normalizePort, onError } = require('./utils')
 
 const port = normalizePort(process.env.PORT || 3000)
 app.set('port', port)
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 
 const server = http.createServer(app)
 server.listen(port)
